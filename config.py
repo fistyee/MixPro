8163b87ea0eac26be81e3b1f88acceecbe7fbc4c
Zhao Qihao <fistyee@users.noreply.github.com>
2023-06-03 14:50:00 +0800

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ _C.DATA = CN()
 # Batch size for a single GPU, could be overwritten by command line argument
 _C.DATA.BATCH_SIZE = 128
 # Path to dataset, could be overwritten by command line argument
-_C.DATA.DATA_PATH = '../../data/Imagenet/'
+_C.DATA.DATA_PATH = '../data/Imagenet/'
     
 # Dataset name
 _C.DATA.DATASET = 'imagenet'
