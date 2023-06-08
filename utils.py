64758548ea939025cf9d7d7bcf5fa51abf6397a5
Zhao Qihao <fistyee@users.noreply.github.com>
2023-06-08 12:50:00 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,3 @@
-# --------------------------------------------------------
-# Swin Transformer
-# Copyright (c) 2021 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Ze Liu
-# --------------------------------------------------------
 
 import os
 import torch
